e984b01c423d0e130a94ad4a448958b1220c11e7
Alex <dgirolamo777@gmail.com>
2020-12-17 21:49:16 +0300

    "terra"

--- src/Test666.java
+++ src/Test666.java
@@ -2,5 +2,6 @@ public class Test666 {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
         System.out.println("Hello, Java!");
+        System.out.println("Hello, Javag!");
     }
 }
